f69bfb7e14f7ad0b2e91293eaacfbb0587bd33cf
Garrett Anderson <gaanderson@gaanderson-macbookpro.roam.corp.google.com>
2016-08-29 16:33:20 -0700

    Finished models, added GET endpoints.

--- codesf_suggest/api.py
+++ codesf_suggest/api.py
@@ -11,3 +11,81 @@ from .main import app
 from .database import session
 
 
+### Define the API endpoints
+############################
+# GET endpoints
+############################
+
+
+def check_post_id(post_id):
+    post = session.query(models.Post).get(post_id)
+    if not post:
+        message = "Could not find post with id {}".format(post_id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+def check_user_id(user_id):
+    user = session.query(models.User).get(user_id)
+    if not user:
+        message = "Could not find user with id {}".format(user_id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+@app.route("api/posts", methods=["GET"])
+@app.route("api/users/<int:user_id>/posts", methods=["GET"])
+@decorators.accept("application/json")
+def posts_get(user_id=None):
+    """ Returns a list of posts """
+
+    if user_id:
+        check_user_id(user_id)
+
+    posts = session.query(models.Post)
+    posts = posts.order_by(models.Post.id)
+
+    if not posts:
+        message = "No posts in database."
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    data = json.dumps([post.as_dictionary() for post in posts],
+        default=json_serial)
+    return Response(data, 200, mimetype="application/json")
+
+@app.route("api/posts/<int:post_id>" methods=["GET"])
+@decorators.accept("application/json")
+def post_get(post_id):
+    """ Returns a specific post """
+
+    check_post_id(post_id)
+
+    post = session.query(models.Post).get(post_id)
+
+    # Check for post's existence
+    if not post:
+        message = "Could not find post with id {}".format(post_id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    data = json.dumps(post.as_dictionary(), default=json_serial)
+    return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/users/<int:user_id>", methods=["GET"])
+@decorators.accept("application/json")
+def user_get(user_id):
+    """ Returns User data """
+
+    check_user_id(user_id)
+
+    user = session.query(models.User).get(user_id)
+
+    if not user:
+        message = "Could not find user with id #{}".format(user_id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    data = json.dumps(user.as_dictionary(), default=json_serial)
+    return Response(data, 200, mimetype="application/json")
+
+
+
--- codesf_suggest/models.py
+++ codesf_suggest/models.py
@@ -1,8 +1,9 @@
 import os.path
 
-from flask.json import jsonify
+import datetime
 
 from flask import url_for
+from flask.json import jsonify
 from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, Sequence, ForeignKey, Enum, 
 from sqlalchemy.orm import relationship, validates, column_property, backref
 
@@ -18,12 +19,27 @@ class User(Base):
     organization = Column(Text)
     position = Column(Text)
     description = Column(Text)
-    start_date = Column(DateTime)
+    start_date = Column(DateTime, default=datetime.datetime.utcnow())
+    last_modified = Column(DateTime, onupdate=datetime.datetime.utcnow())
+    image = Column(Text)
 
     # Foreign relationships
     posts = relationship("Post", backref="admin", cascade="all, delete-orphan")
     volunteered_posts = relationship("Post", backref="user")
 
+    def as_dictionary(self):
+        user_dict = {
+        "id": self.id,
+        "name": self.name,
+        "email": self.email,
+        "organization": self.organization,
+        "position": self.position,
+        "description": self.description,
+        "start_date": self.start_date,
+        "last_modified": self.last_modified,
+        "image": self.image,
+        }
+        return user_dict
 
 class Post(Base):
     """ Base Post Class """
@@ -34,9 +50,27 @@ class Post(Base):
     long_description = Column(Text)
     organization = Column(Text)
     image = Column(Text)
+    start_date = Column(DateTime, default=datetime.datetime.utcnow())
+    last_modified = Column(DateTime, onupdate=datetime.datetime.utcnow())
+
 
     # Foreign relationships
     admin_id = Column(Integer, ForeignKey("User.id"))
+    volunteers = relationship("User")
+
+    def as_dictionary(self):
+        post_dict = {
+        "id": self.id,
+        "title": self.title,
+        "short_description": self.short_description,
+        "long_description": self.long_description,
+        "organization": self.organization,
+        "image": self.image,
+        "start_date": self.start_date,
+        "last_modified": self.last_modified,
+        "admin_id": self.admin_id,
+        }
+
 
 
 
